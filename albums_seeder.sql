2add327ddb7c1c57dc328b4a62a791414b0a03ab
nestor salinas <nestorasm88@gmail.com>
2018-05-22 14:21:33 -0500

    chore: completed practice table

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist,record_name, release_date, sales,genre)
+VALUES ('Michael Jackson','Thriller', '	1982','47.3', 'rock'),
+       ('Eagles','Their Greatest Hits ', '1976','32.2', 'rock'),
+       ('Pink Floyd','The Dark Side of the Moon', '1973	','24.2', 'rock'),
+       ('AC/DC','Back in Black', '1980','26.1', 'rock');
