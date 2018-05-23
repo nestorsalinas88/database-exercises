6af2780e4d0255c338f5f76522218ac551b42311
nestor salinas <nestorasm88@gmail.com>
2018-05-23 17:43:39 -0500

    created: table and specifications

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,18 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
 
-CREATE TABLE IF NOT EXISTS albums(
+CREATE TABLE albums(
   id INT unsigned NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(120),
-  record_name VARCHAR(120) NOT NULL,
+  artist VARCHAR(128),
+  name VARCHAR(128),
   release_date INT(4),
   sales FLOAT(6,2),
-  genre VARCHAR(20) NOT NULL,
+  genre VARCHAR (128),
+
+
+
+
   PRIMARY KEY (id)
 );
 
