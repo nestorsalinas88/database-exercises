6231ba2a1b345226d38d429a415e53e26603d7bd
nestor salinas <nestorasm88@gmail.com>
2018-05-23 16:44:33 -0500

    chore: created query to return age

--- where_exercises.sql
+++ where_exercises.sql
@@ -21,6 +21,9 @@ FROM employees
 WHERE last_name LIKE '%E%'
 ORDER BY emp_no;
 
-
-
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC;
 
