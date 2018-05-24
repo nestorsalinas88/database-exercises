bdb45eb8cf229218a70ec6c9b068504dd71031bf
nestor salinas <nestorasm88@gmail.com>
2018-05-23 22:03:18 -0500

    chore: created query file with SELECT data

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,10 +1,23 @@
 USE codeup_test_db;
 
-SELECT name AS 'all' FROM albums;
+SELECT name_album AS '---->all<-----' FROM albums;
 
-SELECT name_album AS 'released before 1980' FROM albums WHERE release_date < 1980;
+SELECT name_album AS '---->>>released before 1980<<<------' FROM albums WHERE release_date < 1980;
 
-SELECT name_album AS 'by MJ' FROM albums WHERE artist = 'Michael Jackson';
+SELECT name_album AS '--->by MJ<----' FROM albums WHERE artist = 'Michael Jackson';
+
+-- UPDATE albums
+-- SET sales = sales * 10
+
+SELECT * FROM albums;
+
+UPDATE albums
+SET release_date = 1800 WHERE release_date < 1980;
+SELECT  * FROM albums;
+
+UPDATE albums
+SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT * FROM albums;
 
 
 
