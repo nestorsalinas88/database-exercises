f4417c9e648212b763ca14808d3fd903e12a9bed
nestor salinas <nestorasm88@gmail.com>
2018-05-23 19:11:51 -0500

    chore: SELECTED  realese date from table

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,3 +3,5 @@ USE codeup_test_db;
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
 SELECT genre FROM albums WHERE name = 'Bad';
+
+SELECT release_date AS 'Release date of Sgt Peppers Lonely Heart''s Band Club' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
