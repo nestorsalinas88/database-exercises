741187008cd93625339da6dddd9e89f81fd643e7
nestor salinas <nestorasm88@gmail.com>
2018-05-23 19:13:01 -0500

    chore: SELECTED name of albums released between 1990-1999

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,3 +5,5 @@ SELECT name FROM albums WHERE artist = 'Pink Floyd';
 SELECT genre FROM albums WHERE name = 'Bad';
 
 SELECT release_date AS 'Release date of Sgt Peppers Lonely Heart''s Band Club' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT name AS 'Album realesed between 1900 and 1999' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
