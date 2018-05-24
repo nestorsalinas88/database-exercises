85f2b601792b021ef04de3fd932846bab73d9332
nestor salinas <nestorasm88@gmail.com>
2018-05-23 21:21:02 -0500

    chore: refactor code

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
   id INT unsigned NOT NULL AUTO_INCREMENT,
   artist VARCHAR(128),
-  name VARCHAR(128),
+  name_album VARCHAR(128),
   release_date INT(4),
   sales FLOAT(6,2),
   genre VARCHAR (128),
