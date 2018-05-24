59e72a564571ba4f06a787494ff4ca36b8abe9df
nestor salinas <nestorasm88@gmail.com>
2018-05-23 21:23:22 -0500

    chore: created SELECTED according to specifications

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,4 +1,12 @@
 USE codeup_test_db;
 
-SELECT name AS 'dude give me album names: ' FROM albums;
+SELECT name AS 'all' FROM albums;
+
+SELECT name_album AS 'released before 1980' FROM albums WHERE release_date < 1980;
+
+SELECT name_album AS 'by MJ' FROM albums WHERE artist = 'Michael Jackson';
+
+
+
+
 
