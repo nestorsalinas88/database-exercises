b1c41e2b1833fa68b29dbb83da35e9c491761f98
nestor salinas <nestorasm88@gmail.com>
2018-05-29 10:18:16 -0500

    chore: refactor order exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,29 +1,16 @@
 USE employees;
 
-SELECT first_name, last_name
-FROM employees
-WHERE gender = 'M'
-AND (first_name = 'Irena' OR first_name ='Vidya' OR first_name = 'Maya')
-ORDER BY first_name;
+
+-- SELECT CONCAT (first_name, ' ', last_name)
+-- FROM employees
+-- WHERE last_name LIKE 'E%'
+-- AND last_name LIKE '%e'
+-- ORDER BY emp_no DESC;
 
 
-SELECT first_name, last_name  AS '--->>> MORE <<<---'
-FROM employees
-WHERE first_name IN ('Vidya','Irena','Maya')
-ORDER BY first_name, last_name;
 
-SELECT first_name, last_name
+SELECT DATEDIFF(CURDATE(), hire_date) AS '-->>Days worked<<--', CONCAT (first_name, ' ', last_name), birth_date
 FROM employees
-WHERE last_name LIKE 'E%'
-ORDER BY emp_no DESC;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%E%'
-ORDER BY emp_no;
-
-SELECT *
-FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%12-25'
-ORDER BY birth_date, hire_date DESC;
+WHERE birth_date LIKE '19%-12-25'
+AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date ASC, hire_date DESC;
