213ad48c62603fc3d46c56f1e74cf73d96349f19
nestor salinas <nestorasm88@gmail.com>
2018-05-29 14:03:03 -0500

    chore: query that identifies depts with females

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -23,3 +23,18 @@ WHERE emp_no IN (
   WHERE to_date > CURDATE()
 )
 AND gender = 'F';
+
+
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN(
+  SELECT dept_no
+  FROM dept_manager
+  WHERE emp_no IN(
+    SELECT emp_no
+    FROM employees
+    WHERE gender = 'F'
+  )
+    AND to_date > NOW()
+);
