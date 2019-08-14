de4e9db20ca47f54520f42f17524504e0c0e9b3c
nestor salinas <nestorasm88@gmail.com>
2019-08-14 15:11:26 -0500

    MYSQL FUN

---
+++ join_exercises.sql
@@ -0,0 +1,31 @@
+USE employees;
+
+SELECT d.dept_name
+AS 'Department Name', concat(e.first_name, ' ', e.last_name)           AS 'Department Manager'
+FROM employees e
+  JOIN dept_manager de ON de.emp_no = e.emp_no
+  JOIN departments d ON d.dept_no = de.dept_no
+WHERE de.to_date >= curdate()
+ORDER BY d.dept_name;
+
+
+SELECT d.dept_name
+AS 'Department name', concat(e.first_name, ' ', e.last_name)
+AS 'Manager'
+FROM employees e
+  JOIN dept_manager dm ON dm.emp_no = e.emp_no
+  JOIN departments d ON d.dept_no = dm.dept_no
+WHERE e.gender = 'F' AND dm.to_date >= curdate()
+ORDER BY d.dept_name;
+
+
+SELECT t.title, COUNT(t.emp_no)
+AS 'COUNT'
+FROM titles t
+  JOIN dept_emp
+
+
+
+
+
+
---
+++ practice_update.sql
@@ -0,0 +1,20 @@
+USE codeup_db;
+
+SELECT name_album AS '---->all<-----' FROM albums;
+
+SELECT name_album AS '---->>>released before 1980<<<------' FROM albums WHERE release_date < 1980;
+
+SELECT name_album AS '--->by MJ<----' FROM albums WHERE artist = 'Michael Jackson';
+
+-- UPDATE albums
+-- SET sales = sales * 10
+
+SELECT * FROM albums;
+
+UPDATE albums
+SET release_date = 1800 WHERE release_date < 1980;
+SELECT  * FROM albums;
+
+UPDATE albums
+SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT * FROM albums;
