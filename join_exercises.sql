3128590cd8ece7b594a30c5abf3c56e241ad37f1
nestor salinas <nestorasm88@gmail.com>
2019-08-16 15:31:36 -0500

    multiple join examples, aggregated functions, fun examples

--- join_exercises.sql
+++ join_exercises.sql
@@ -19,14 +19,33 @@ FROM employees e
 WHERE e.gender = 'F' AND dm.to_date >= curdate()
 ORDER BY d.dept_name;
 
+select * from employees;
 
-SELECT t.title, COUNT(t.emp_no)
-AS 'COUNT'
-FROM titles t
-  JOIN dept_emp
-
-
-
+select d.dept_name as 'Department #', e.gender as 'GENDER'
+from employees e
+ JOIN dept_manager dm ON dm.emp_no = e.emp_no
+ join departments d ON d.dept_no = dm.dept_no;
 
 
+select t.title, count(t.title)
+from titles t
+  join dept_emp de on de.emp_no = t.emp_no
+  join departments d on de.dept_no = d.dept_no
+  where t.to_date > now()
+  and d.dept_name = 'Customer Service'
+  and de.to_date >= curdate()
+  group by t.title;
 
+
+
+SELECT d.dept_name
+AS 'Department name', concat(e.first_name, ' ', e.last_name)
+AS 'Manager',
+s.salary
+FROM employees e
+  JOIN dept_manager dm ON dm.emp_no = e.emp_no
+  JOIN departments d ON d.dept_no = dm.dept_no
+  join salaries s on e.emp_no = s.emp_no
+WHERE dm.to_date > curdate()
+and s.to_date > curdate()
+ORDER BY d.dept_name;
